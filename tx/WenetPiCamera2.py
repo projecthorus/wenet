75d3c6268fd20017dc3d938c593c3c64b7491113
Mark Jessop <darkside@Marks-MacBook-Pro.local>
2023-02-17 14:22:13 +1030

    Initial stab at picamera2 support

---
+++ tx/WenetPiCamera2.py
@@ -0,0 +1,392 @@
+#!/usr/bin/env python2.7
+#
+#	Wenet PiCamera2 Wrapper Class.
+#	Supports the 'new' libcamera / picamera2 API
+#
+#	Copyright (C) 2023  Mark Jessop <vk5qi@rfhead.net>
+#	Released under GNU GPL v3 or later
+#
+# 	References:
+#		https://github.com/raspberrypi/picamera2
+#		https://datasheets.raspberrypi.com/camera/picamera2-manual.pdf
+
+import glob
+import os
+import datetime
+import time
+import traceback
+
+from picamera2 import Picamera2
+from libcamera import controls, Transform
+from time import sleep
+from threading import Thread
+
+
+
+class WenetPiCamera2(object):
+	""" PiCamera2 Wrapper Class
+
+	Raspberry Pi Camera 2 Image source for Wenet.
+    Uses the new libcamera-based PiCamera2 library.
+	Captures multiple images, picks the best, then 
+	transmits it via a PacketTX object. 
+
+
+	"""
+
+	def __init__(self,
+				callsign = "N0CALL",
+				tx_resolution=(1936,1088), 
+				num_images=1,
+				image_delay=0.5, 
+				vertical_flip = False, 
+				horizontal_flip = False,
+				greyworld = False,
+                lens_position = 0.0,
+				temp_filename_prefix = 'picam_temp',
+				debug_ptr = None
+				):
+
+		""" Instantiate a WenetPiCam Object
+			used to capture images from a PiCam using 'optimal' capture techniques.
+
+			Keyword Arguments:
+			callsign: The callsign to be used when converting images to SSDV. Must be <=6 characters in length.
+			tx_resolution: Tuple (x,y) containing desired image *transmit* resolution.
+						NOTE: both x and y need to be multiples of 16 to be used with SSDV.
+						NOTE: This will resize with NO REGARD FOR ASPECT RATIO - it's up to you to get that right.
+
+			num_images: Number of images to capture in sequence when the 'capture' function is called.
+						The 'best' (largest filesize) image is selected and saved.
+			image_delay: Delay time (seconds) between each captured image.
+
+			vertical_flip: Flip captured images vertically.
+			horizontal_flip: Flip captured images horizontally.
+							Used to correct for picam orientation.
+            greyworld: Use Greyworld AWB setting, for IR-filtered images.
+            lens_position: Lens Position setting (float), 0.0 = Infinity, 10 = very close.
+                   Only usable on Pi Camera v3 modules.
+
+			temp_filename_prefix: prefix used for temporary files.
+
+			debug_ptr:	'pointer' to a function which can handle debug messages.
+						This function needs to be able to accept a string.
+						Used to get status messages into the downlink.
+
+		"""
+
+		self.debug_ptr = debug_ptr
+		self.temp_filename_prefix = temp_filename_prefix
+		self.num_images = num_images
+		self.image_delay = image_delay
+		self.callsign = callsign
+		self.tx_resolution = tx_resolution
+		self.src_resolution = src_resolution
+		self.horizontal_flip = horizontal_flip
+		self.vertical_flip = vertical_flip
+		self.greyworld = greyworld
+        self.lens_position = lens_position
+
+		self.init_camera()
+
+
+	def init_camera(self):
+		# Attempt to start picam.
+		self.cam = Picamera2()
+
+        self.camera_properties = self.cam.camera_properties
+
+        self.src_resolution = self.camera_properties['PixelArraySize']
+
+		# Configure camera.
+        capture_config = picam2.create_still_configuration(
+            transform=Transform(hflip=self.horizontal_flip, vflip=self.vertical_flip)
+        )
+        self.cam.configure(capture_config)
+
+        self.cam.set_controls(
+            {'AwbMode': controls.AwbModeEnum.Daylight,
+            'AeMeteringMode', controls.AeMeteringModeEnum.Matrix}
+            )
+
+        # Set Pi Camera 3 lens position
+        if 'LensPostion' in self.cam.camera_controls:
+            self.cam.set_controls({"AfMode": controls.AfModeEnum.Manual, "LensPosition": self.lens_position})
+
+		# Start the 'preview' mode, effectively opening the 'shutter'.
+		# This lets the camera gain control algs start to settle.
+		self.cam.start()
+
+	def debug_message(self, message):
+		""" Write a debug message.
+		If debug_ptr was set to a function during init, this will
+		pass the message to that function, else it will just print it.
+		This is used mainly to get updates on image capture into the Wenet downlink.
+
+		"""
+		message = "PiCam Debug: " + message
+		if self.debug_ptr != None:
+			self.debug_ptr(message)
+		else:
+			print(message)
+
+	def close(self):
+		self.cam.stop()
+		self.cam.close()
+
+	def capture(self, filename='picam.jpg', quality=90, bayer=False):
+		""" Capture an image using the PiCam
+			
+			Keyword Arguments:
+			filename:	destination filename.
+		"""
+
+        self.cam.options['quality'] = quality
+
+		# Attempt to capture a set of images.
+		for i in range(self.num_images):
+			self.debug_message("Capturing Image %d of %d" % (i+1,self.num_images))
+			# Wrap this in error handling in case we lose the camera for some reason.
+			try:
+                self.cam.capture_file("%s_%d.jpg" % (self.temp_filename_prefix,i))
+				print(f"Image captured: {time.time()}")
+				if self.image_delay > 0:
+					sleep(self.image_delay)
+			except Exception as e: # TODO: Narrow this down...
+				self.debug_message("Capture Error: %s" % str(e))
+				# Immediately return false. Not much point continuing to try and capture images.
+				return False
+
+		
+		# Otherwise, continue to pick the 'best' image based on filesize.
+		self.debug_message("Choosing Best Image.")
+		pic_list = glob.glob("%s_*.jpg" % self.temp_filename_prefix)
+		pic_sizes = []
+		# Iterate through list of images and get the file sizes.
+		for pic in pic_list:
+			pic_sizes.append(os.path.getsize(pic))
+		largest_pic = pic_list[pic_sizes.index(max(pic_sizes))]
+
+		# Copy best image to target filename.
+		self.debug_message("Copying image to storage with filename %s" % filename)
+		os.system("cp %s %s" % (largest_pic, filename))
+		# Clean up temporary images.
+		os.system("rm %s_*.jpg" % self.temp_filename_prefix)
+
+		return True 
+
+	def ssdvify(self, filename="output.jpg", image_id=0, quality=6):
+		""" Convert a supplied JPEG image to SSDV.
+		Returns the filename of the converted SSDV image.
+
+		Keyword Arguments:
+		filename:	Source JPEG filename.
+					Output SSDV image will be saved to to a temporary file (webcam_temp.jpg) which should be
+					transmitted immediately.
+		image_id:	Image ID number. Must be incremented between images.
+		quality:	JPEG quality level: 4 - 7, where 7 is 'lossless' (not recommended).
+					6 provides good quality at decent file-sizes.
+
+		"""
+
+		# Wrap image ID field if it's >255.
+		image_id = image_id % 256
+
+		# Resize image to the desired resolution.
+		self.debug_message("Resizing image.")
+		return_code = os.system("convert %s -resize %dx%d\! picam_temp.jpg" % (filename, self.tx_resolution[0], self.tx_resolution[1]))
+		if return_code != 0:
+			self.debug_message("Resize operation failed!")
+			return "FAIL"
+
+		# Get non-extension part of filename.
+		file_basename = filename[:-4]
+
+		# Construct SSDV command-line.
+		ssdv_command = "ssdv -e -n -q %d -c %s -i %d picam_temp.jpg picam_temp.ssdv" % (quality, self.callsign, image_id)
+		print(ssdv_command)
+		# Update debug message.
+		self.debug_message("Converting image to SSDV.")
+
+		# Run SSDV converter.
+		return_code = os.system(ssdv_command)
+
+		if return_code != 0:
+			self.debug_message("ERROR: Could not perform SSDV Conversion.")
+			return "FAIL"
+		else:
+			return "picam_temp.ssdv"
+
+	auto_capture_running = False
+	def auto_capture(self, destination_directory, tx, post_process_ptr=None, delay = 0, start_id = 0):
+		""" Automatically capture and transmit images in a loop.
+		Images are automatically saved to a supplied directory, with file-names
+		defined using a timestamp.
+
+		Use the run() and stop() functions to start/stop this running.
+		
+		Keyword Arguments:
+		destination_directory:	Folder to save images to. Both raw JPEG and SSDV images are saved here.
+		tx:		A reference to a PacketTX Object, which is used to transmit packets, and interrogate the TX queue.
+		post_process_ptr: An optional function which is called after the image is captured. This function
+						  will be passed the path/filename of the captured image.
+						  This can be used to add overlays, etc to the image before it is SSDVified and transmitted.
+						  NOTE: This function need to modify the image in-place.
+		delay:	An optional delay in seconds between capturing images. Defaults to 0.
+				This delay is added on top of any delays caused while waiting for the transmit queue to empty.
+		start_id: Starting image ID. Defaults to 0.
+		"""
+
+		image_id = start_id
+
+		while self.auto_capture_running:
+			# Sleep before capturing next image.
+			sleep(delay)
+
+			# Grab current timestamp.
+			capture_time = datetime.datetime.utcnow().strftime("%Y%m%d-%H%M%SZ")
+			capture_filename = destination_directory + "/%s_picam.jpg" % capture_time
+
+			# Attempt to capture.
+			capture_successful = self.capture(capture_filename)
+
+			# If capture was unsuccessful, try again in a little bit
+			if not capture_successful:
+				sleep(5)
+
+				self.debug_message("Capture failed! Attempting to reset camera...")
+
+				try:
+					self.cam.stop()
+					self.cam.close()
+				except:
+					self.debug_message("Closing camera object failed.")
+
+				try:
+					self.init_camera()
+				except:
+					self.debug_message("Error initializing camera!")
+					sleep(1)
+
+				continue
+
+			# Otherwise, proceed to post-processing step.
+			if post_process_ptr != None:
+				try:
+					self.debug_message("Running Image Post-Processing")
+					post_process_ptr(capture_filename)
+				except:
+					error_str = traceback.format_exc()
+					self.debug_message("Image Post-Processing Failed: %s" % error_str)
+
+			# SSDV'ify the image.
+			ssdv_filename = self.ssdvify(capture_filename, image_id=image_id)
+
+			# Check the SSDV Conversion has completed properly. If not, continue
+			if ssdv_filename == "FAIL":
+				sleep(1)
+				continue
+
+
+			# Otherwise, read in the file and push into the TX buffer.
+			file_size = os.path.getsize(ssdv_filename)
+
+			# Wait until the transmit queue is empty before pushing in packets.
+			self.debug_message("Waiting for SSDV TX queue to empty.")
+			while tx.image_queue_empty() == False:
+				sleep(0.05) # Sleep for a short amount of time.
+				if self.auto_capture_running == False:
+					return
+
+			# Inform ground station we are about to send an image.
+			self.debug_message("Transmitting %d PiCam SSDV Packets." % (file_size//256))
+
+			# Push SSDV file into transmit queue.
+			tx.queue_image_file(ssdv_filename)
+
+			# Increment image ID.
+			image_id = (image_id + 1) % 256
+		# Loop!
+
+		self.debug_message("Uh oh, we broke out of the main thread. This is not good!")
+
+
+	def run(self, destination_directory, tx, post_process_ptr=None, delay = 0, start_id = 0):
+		""" Start auto-capturing images in a thread.
+
+		Refer auto_capture function above.
+		
+		Keyword Arguments:
+		destination_directory:	Folder to save images to. Both raw JPEG and SSDV images are saved here.
+		tx:		A reference to a PacketTX Object, which is used to transmit packets, and interrogate the TX queue.
+		post_process_ptr: An optional function which is called after the image is captured. This function
+						  will be passed the path/filename of the captured image.
+						  This can be used to add overlays, etc to the image before it is SSDVified and transmitted.
+						  NOTE: This function need to modify the image in-place.
+		delay:	An optional delay in seconds between capturing images. Defaults to 0.
+				This delay is added on top of any delays caused while waiting for the transmit queue to empty.
+		start_id: Starting image ID. Defaults to 0.
+		"""		
+
+		self.auto_capture_running = True
+
+		capture_thread = Thread(target=self.auto_capture, kwargs=dict(
+			destination_directory=destination_directory,
+			tx = tx,
+			post_process_ptr=post_process_ptr,
+			delay=delay,
+			start_id=start_id))
+
+		capture_thread.start()
+
+	def stop(self):
+		self.auto_capture_running = False
+
+	# TODO: Non-blocking image capture.
+	capture_finished = False
+	def trigger_capture():
+		pass
+
+
+# Basic transmission test script.
+if __name__ == "__main__":
+	import PacketTX
+	import argparse
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument("callsign", default="N0CALL", help="Payload Callsign")
+	parser.add_argument("--txport", default="/dev/ttyAMA0", type=str, help="Transmitter serial port. Defaults to /dev/ttyAMA0")
+	parser.add_argument("--baudrate", default=115200, type=int, help="Transmitter baud rate. Defaults to 115200 baud.")
+	args = parser.parse_args()
+
+	callsign = args.callsign
+	print("Using Callsign: %s" % callsign)
+
+	def post_process(filename):
+		print("Doing nothing with %s" % filename)
+
+	tx = PacketTX.PacketTX(serial_port=args.txport, serial_baud=args.baudrate, callsign=callsign)
+	tx.start_tx()
+
+
+	picam = WenetPiCam(src_resolution=(1920,1088), 
+		tx_resolution=(1920,1088), 
+		callsign=callsign, 
+		num_images=5, 
+		debug_ptr=tx.transmit_text_message, 
+		vertical_flip=False, 
+		horizontal_flip=False)
+
+	picam.run(destination_directory="./tx_images/", 
+		tx = tx,
+		post_process_ptr = post_process
+		)
+	try:
+		while True:
+			tx.transmit_text_message("Waiting...")
+			sleep(5)
+	except KeyboardInterrupt:
+		print("Closing")
+		picam.stop()
+		tx.close()
+
---
+++ tx/tx_picamera2_gps.py
@@ -0,0 +1,196 @@
+#!/usr/bin/env python
+#
+#	PiCamera2 Library Transmitter Script - with GPS Data and Logo Overlay.
+#	Capture images from the PiCam, and transmit them.
+#
+#	Copyright (C) 2023  Mark Jessop <vk5qi@rfhead.net>
+#	Released under GNU GPL v3 or later
+#
+
+import PacketTX
+import WenetPiCamera2
+import ublox
+import argparse
+import time
+import os
+import subprocess
+import traceback
+
+parser = argparse.ArgumentParser()
+parser.add_argument("callsign", default="N0CALL", help="Payload Callsign")
+parser.add_argument("--gps", default="/dev/ttyACM0", help="uBlox GPS Serial port. Defaults to /dev/ttyACM0")
+parser.add_argument("--logo", default="none", help="Optional logo to overlay on image.")
+parser.add_argument("--txport", default="/dev/ttyAMA0", type=str, help="Transmitter serial port. Defaults to /dev/ttyAMA0")
+parser.add_argument("--baudrate", default=115200, type=int, help="Transmitter baud rate. Defaults to 115200 baud.")
+args = parser.parse_args()
+
+callsign = args.callsign
+# Truncate callsign if it's too long.
+if len(callsign) > 6:
+	callsign = callsign[:6]
+
+print("Using Callsign: %s" % callsign)
+
+
+# Start up Wenet TX.
+tx = PacketTX.PacketTX(serial_port=args.txport, serial_baud=args.baudrate, callsign=callsign, log_file="debug.log", udp_listener=55674)
+tx.start_tx()
+
+# Sleep for a second to let the transmitter fire up.
+time.sleep(1)
+
+# Initialise a couple of global variables.
+max_altitude = 0
+system_time_set = False
+
+# Disable Systemctl NTP synchronization so that we can set the system time on first GPS lock.
+# This is necessary as NTP will refuse to sync the system time to the information we feed it via ntpshm unless
+# the system clock is already within a few seconds.
+ret_code = os.system("timedatectl set-ntp 0")
+if ret_code == 0:
+	tx.transmit_text_message("GPS Debug: Disabled NTP Sync until GPS lock.")
+else:
+	tx.transmit_text_message("GPS Debug: Could not disable NTP sync.")
+
+def handle_gps_data(gps_data):
+	""" Handle GPS data passed to us from the UBloxGPS instance """
+	global max_altitude, tx, system_time_set
+
+	# Immediately generate and transmit a GPS packet.
+	tx.transmit_gps_telemetry(gps_data)
+
+	# If we have GPS fix, update the max altitude field.
+	if (gps_data['altitude'] > max_altitude) and (gps_data['gpsFix'] == 3):
+		max_altitude = gps_data['altitude']
+
+	# If we have GPS lock, set the system clock to it. (Only do this once.)
+	if (gps_data['gpsFix'] == 3) and not system_time_set:
+		dt = gps_data['datetime']
+		try:
+			new_time = dt.strftime('%Y-%m-%d %H:%M:%S')
+			ret_code = os.system("timedatectl set-time \"%s\"" % new_time)
+			if ret_code == 0:
+				tx.transmit_text_message("GPS Debug: System clock set to GPS time %s" % new_time)
+			else:
+				tx.transmit_text_message("GPS Debug: Attempt to set system clock failed!")
+			system_time_set = True
+
+			# Re-enable NTP synchronisation
+			ret_code = os.system("timedatectl set-ntp 1")
+			if ret_code == 0:
+				tx.transmit_text_message("GPS Debug: Re-enabled NTP sync.")
+			else:
+				tx.transmit_text_message("GPS Debug: Could not enable NTP sync.")
+		except:
+			tx.transmit_text_message("GPS Debug: Attempt to set system clock failed!")
+
+
+
+# Try and start up the GPS rx thread.
+try:
+	gps = ublox.UBloxGPS(port=args.gps, 
+		dynamic_model = ublox.DYNAMIC_MODEL_AIRBORNE1G, 
+		update_rate_ms = 1000,
+		debug_ptr = tx.transmit_text_message,
+		callback = handle_gps_data,
+		log_file = 'gps_data.log'
+		)
+except Exception as e:
+	tx.transmit_text_message("ERROR: Could not Open GPS - %s" % str(e), repeats=5)
+	gps = None
+
+# Define our post-processing callback function, which gets called by WenetPiCam
+# after an image has been captured.
+def post_process_image(filename):
+	""" Post-process the image, adding on Logo overlay and GPS data if requested. """
+	global gps, max_altitude, args, tx
+
+	# Try and grab current GPS data snapshot
+	try:
+		if gps != None:
+			gps_state = gps.read_state()
+
+			# Format time
+			short_time = gps_state['datetime'].strftime("%Y-%m-%d %H:%M:%S")
+
+			# Construct string which we will add onto the image.
+			if gps_state['numSV'] < 3:
+				# If we don't have enough sats for a lock, don't display any data.
+				# TODO: Use the GPS fix status values here instead.
+				gps_string = "No GPS Lock"
+			else:
+				gps_string = "%s Lat: %.5f   Lon: %.5f  Alt: %dm (%dm)  Speed: H %03.1f kph  V %02.1f m/s" % (
+					short_time,
+					gps_state['latitude'],
+					gps_state['longitude'],
+					int(gps_state['altitude']),
+					int(max_altitude),
+					gps_state['ground_speed'],
+					gps_state['ascent_rate'])
+		else:
+			gps_string = ""
+	except:
+		error_str = traceback.format_exc()
+		tx.transmit_text_message("GPS Data Access Failed: %s" % error_str)
+		gps_string = ""
+
+	# Build up our imagemagick 'convert' command line
+	overlay_str = "convert %s -gamma 0.8 -font Helvetica -pointsize 30 -gravity North " % filename 
+	overlay_str += "-strokewidth 2 -stroke '#000C' -annotate +0+5 \"%s\" " % gps_string
+	overlay_str += "-stroke none -fill white -annotate +0+5 \"%s\" " % gps_string
+	# Add on logo overlay argument if we have been given one.
+	if args.logo != "none":
+		overlay_str += "%s -gravity SouthEast -composite " % args.logo
+
+	overlay_str += filename
+
+	tx.transmit_text_message("Adding overlays to image.")
+	os.system(overlay_str)
+
+	return
+
+
+# Finally, initialise the PiCam capture object.
+picam = WenetPiCamera2.WenetPiCamera2( 
+		tx_resolution=(1936,1088), 
+		callsign=callsign, 
+		num_images=5, 
+		debug_ptr=tx.transmit_text_message, 
+		vertical_flip=False, 
+		horizontal_flip=False)
+# .. and start it capturing continuously.
+picam.run(destination_directory="./tx_images/", 
+	tx = tx,
+	post_process_ptr = post_process_image
+	)
+
+
+# Main 'loop'.
+try:
+	while True:
+		# Do nothing!
+		# Sleep to avoid chewing up CPU cycles in this loop.
+		time.sleep(1)
+# Catch CTRL-C, and exit cleanly.
+# Only really used during debugging.
+except KeyboardInterrupt:
+	print("Closing")
+	picam.stop()
+	tx.close()
+	gps.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
