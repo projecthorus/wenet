a92b08f8da786d0dcf0accbfecc2f2ee82f7b1bc
Andrew Koenig <ke5gdb@gmail.com>
2024-10-04 00:01:53 -0500

    Minor adjustments in start_rx_docker.sh and general cleanup

--- rx/start_rx_docker.sh
+++ rx/start_rx_docker.sh
@@ -28,8 +28,8 @@ fi
 : "${IMAGE_PORT:=7890}"
 
 # Start up the SSDV Uploader script and push it into the background.
-#python3 ssdvuploader.py "$MYCALL" &
-#SSDV_UPLOAD_PID=$!
+python3 ssdvuploader.py "$MYCALL" &
+SSDV_UPLOAD_PID=$!
 
 # Start the Web Interface Server
 if [ "$UDP_PORT" = "0" ]; then
@@ -65,24 +65,16 @@ if [ "$SDR_TYPE" = "RTLSDR" ] ; then
   ./drs232_ldpc - -  -vv 2> /dev/null | \
   python3 rx_ssdv.py --partialupdate 16 --headless
 elif [ "$SDR_TYPE" = "KA9Q" ] ; then
-  # Establish a channel
-  #  echo -n "Tuning receiver -- "
-  #  avahi-resolve-host-name "$DEVICE"
-  #  timeout 10 tune --samprate "$SDR_RATE" --mode wenet --frequency "$RX_SSB_FREQ" --ssrc "$RX_SSB_FREQ" --radio "$DEVICE"
-
   # Start receiver
   echo "Starting pcmcat and demodulator"
-  #  PCMDEVICE="$DEVICE" #$(echo "$DEVICE" | sed 's/.local/-pcm.local/g')
-  #  echo "$PCMDEVICE"
-  #  pcmcat -s "$RX_SSB_FREQ" "$PCMDEVICE" | \
   pcmcat "$DEVICE" | \
-  ./fsk_demod --cs16 -s --stats=100 2 "$SDR_RATE" "$BAUD_RATE" - - 2> >(python3 fskstatsudp.py --rate 1 --freq $RX_SSB_FREQ --samplerate $SDR_RATE) | \
+  ./fsk_demod --cs16 -s --stats=100 2 "$SDR_RATE" "$BAUD_RATE" - - 2> >(python3 fskstatsudp.py --rate 1 --freq $RX_SSB_FREQ --samplerate $SDR_RATE --image_port $IMAGE_PORT) | \
   ./drs232_ldpc - -  -vv 2> /dev/null | \
-  python3 rx_ssdv.py --partialupdate 16 --headless
+  python3 rx_ssdv.py --partialupdate 16 --headless --image_port $IMAGE_PORT
 else
   echo "No valid SDR type specified! Please enter RTLSDR or KA9Q!"
 fi
 
 # Kill off the SSDV Uploader and the GUIs
-#kill $SSDV_UPLOAD_PID
+kill $SSDV_UPLOAD_PID
 kill $WEB_VIEWER_PID
